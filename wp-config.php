6279cf909eb0ad6464815b3ce0f3e0b0f9a9f4f7
Patrick Zhang <PatDuJour@gmail.com>
2016-03-21 12:03:43 -0700

    Update backend pointer

--- wp-config.php
+++ wp-config.php
@@ -20,7 +20,7 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'hillotask_web');
+define('DB_NAME', 'hillotask');
 
 /** MySQL database username */
 define('DB_USER', 'root');
